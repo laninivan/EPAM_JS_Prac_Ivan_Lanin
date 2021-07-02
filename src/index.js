ade7dbcadcefbc3f35c6578b91c1faf1c75e9fe4
Lanin Ivan <lanin.ivan1@gmail.com>
2021-07-02 20:42:54 +0400

    Commit_02_07_2021

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 console.log('Hello World!')
 
 import './styles/style.scss'
+
+
+let batAllVoi=document.getElementById('button1')
+let batMicro=document.getElementById('button2')
+let batStream=document.getElementById('button3')
+let InserTxt=document.querySelector('.sum>h1')
+
+batAllVoi.addEventListener('click',function(){
+    InserTxt.innerHTML='All voice: Active'
+    document.querySelector('#button1').style.backgroundColor="#F0E68C";
+    document.querySelector('#button2').style.backgroundColor="white";
+    document.querySelector('#button3').style.backgroundColor="white";
+})
+
+batMicro.addEventListener('click',function(){
+    InserTxt.innerHTML='Microphone: Active'
+    document.querySelector('#button2').style.backgroundColor="#F0E68C";
+    document.querySelector('#button1').style.backgroundColor="white";
+    document.querySelector('#button3').style.backgroundColor="white";
+})
+
+batStream.addEventListener('click',function(){
+    InserTxt.innerHTML='Stream: Active'
+    document.querySelector('#button3').style.backgroundColor="#F0E68C";
+    document.querySelector('#button1').style.backgroundColor="white";
+    document.querySelector('#button2').style.backgroundColor="white";
+})
+
+
